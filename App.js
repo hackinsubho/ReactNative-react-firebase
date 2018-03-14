cadafd2a3d9fddf5921b2df9b4e07bf097cc6083
hackinsubho <subho3010@gmail.com>
2018-03-14 23:36:15 +0530

    email authentication from firebase

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWa2yQef7M7xUiTW10srZPx9VFgYHpNIY",
+  authDomain: "react-firebase-535c8.firebaseapp.com",
+  databaseURL: "https://react-firebase-535c8.firebaseio.com",
+  projectId: "react-firebase-535c8",
+  storageBucket: "react-firebase-535c8.appspot.com",
+  
+};
+firebase.initializeApp(firebaseConfig);
+
+import { Container, Content, Header, Form , Input, Item, Button, Label, Icon } from 'native-base';
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      email :'',
+      password: ''
+    }
+
+  }
+
+
+  signUpUser = (email, password) => {
+      try{
+        if (this.state.password.length< 6){
+          alert("Please enter atleast 6 charecters.")
+          return;
+        }
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+      }
+      catch(error){
+        console.log(error.toString())
+      }
+  }
+
+  loginUser = (email, password) => {
+    try{
+       firebase.auth().signInWithEmailAndPassword(email,password).then(function(user){
+         console.log(user)
+       })
+    }
+    catch(error){
+      console.log(error.toString())
+    }
+  }
+
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+    
+      <Container style={styles.container}>
+        <Form>
+          <Item floatingLabel>
+          <Label>Email</Label>
+          <Input autoCorrect= {false} autoCapitalize="none" onChangeText={(email)=> this.setState({email})} />
+          </Item>
+          <Item floatingLabel>
+          <Label>Password</Label>
+          <Input autoCorrect= {false} autoCapitalize="none" secureTextEntry={true} onChangeText={(password)=> this.setState({password})}/>
+          </Item>
+          <Button rounded full success style={{ marginTop: 10}} onPress={()=> this.loginUser(this.state.email, this.state.password)}>
+          <Text style={{ color: 'white'}}>Login</Text>
+          </Button>
+          <Button rounded full primary style={{ marginTop: 10}} onPress={()=> this.signUpUser(this.state.email, this.state.password)}>
+          <Text style={{ color: 'white'}} >SignUp</Text>
+          </Button>
+          </Form>
+       </Container> 
+      
     );
   }
 }
@@ -15,7 +84,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    padding : 10,
     justifyContent: 'center',
   },
 });
